b4906cb7256116854bd326043e8e56cf9b1a6eb5
Christoph Flick <christophflick@gmx.de>
2020-02-23 10:15:18 +0100

    Add example APIResponse description

--- manage-vehicle-data/src/main/kotlin/de/flick/traffic/TrafficController.kt
+++ manage-vehicle-data/src/main/kotlin/de/flick/traffic/TrafficController.kt
@@ -3,13 +3,17 @@ package de.flick.traffic
 import de.flick.traffic.vehicles.VehicleDTO
 import de.flick.traffic.vehicles.VehicleRepository
 import mu.KotlinLogging
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType.ARRAY
+import org.eclipse.microprofile.openapi.annotations.media.Content
+import org.eclipse.microprofile.openapi.annotations.media.Schema
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse
 import javax.annotation.security.PermitAll
 import javax.enterprise.context.RequestScoped
 import javax.inject.Inject
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
-import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.MediaType.APPLICATION_JSON
 
 
 private val logger = KotlinLogging.logger {}
@@ -23,6 +27,15 @@ constructor(private val vehicleRepository: VehicleRepository) {
     @Path("/vehicles")
     @GET()
     @PermitAll
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces(APPLICATION_JSON)
+    @APIResponse(
+        responseCode = "200",
+        content = [Content(
+            mediaType = APPLICATION_JSON,
+            schema = Schema(
+                implementation = VehicleDTO::class,
+                type = ARRAY)
+        )]
+    )
     fun getVehicles(): List<VehicleDTO> = vehicleRepository.findByMinutesFromNow(5)
 }
