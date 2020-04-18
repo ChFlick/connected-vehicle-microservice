0f607bc208c83b3bf0c8b3789ea0fa6218aafbae
Christoph Flick <christophflick@gmx.de>
2020-04-18 10:22:45 +0200

    Try fixing availability test cases

--- manage-vehicle-data/src/test/kotlin/de/flick/traffic/TrafficControllerTest.kt
+++ manage-vehicle-data/src/test/kotlin/de/flick/traffic/TrafficControllerTest.kt
@@ -1,20 +1,47 @@
 package de.flick.traffic
 
+import de.flick.connectors.InfluxDBResource
+import io.quarkus.test.common.QuarkusTestResource
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.given
 import org.hamcrest.CoreMatchers.notNullValue
 import org.junit.jupiter.api.Test
+import java.time.ZonedDateTime
+import javax.ws.rs.core.HttpHeaders.ACCEPT
+import javax.ws.rs.core.MediaType.APPLICATION_JSON
+import javax.ws.rs.core.Response.Status.OK
 
 @QuarkusTest
+@QuarkusTestResource(InfluxDBResource::class)
 class TrafficControllerTest {
 
     @Test
-    fun getVehicles() {
+    fun carsSinceLastFiveSeconds_IsAvailable() {
+        // @formatter:off
         given()
-            .`when`()
-                .get("/traffic/vehicles")
-            .then()
-                .statusCode(200)
-                .body(notNullValue())
+            .header(ACCEPT, APPLICATION_JSON)
+        .`when`()
+            .get("/vehicles/carsSinceLastFiveSeconds")
+        .then()
+            .statusCode(OK.statusCode)
+            .body(notNullValue())
+        // @formatter:on
+    }
+
+    @Test
+    fun busesBetween_IsAvailable() {
+        // @formatter:off
+        given()
+            .header(ACCEPT, APPLICATION_JSON)
+            .params(mapOf(
+                "start" to ZonedDateTime.now().minusDays(1),
+                "end" to ZonedDateTime.now()
+            ))
+        .`when`()
+            .get("/vehicles/busesBetween")
+        .then()
+            .statusCode(OK.statusCode)
+            .body(notNullValue())
+        // @formatter:on
     }
 }
