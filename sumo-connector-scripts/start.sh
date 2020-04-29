8a0fb6e58605802f9c31c462d10d4d557e093032
Christoph Flick <christophflick@gmx.de>
2020-04-29 12:29:03 +0200

    Add -u to python to see logs in docker output

--- sumo-connector-scripts/start.sh
+++ sumo-connector-scripts/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 "$SCRIPT_FOLDER"/sendSimulationDataToLocalInflux.py "$SCRIPT_FOLDER"/MoSTScenario-"$MOST_VERSION"/scenario/most.traci.sumocfg "$INFLUX_HOST" "$INFLUX_PORT"
+python3 -u "$SCRIPT_FOLDER"/sendSimulationDataToLocalInflux.py "$SCRIPT_FOLDER"/MoSTScenario-"$MOST_VERSION"/scenario/most.traci.sumocfg "$INFLUX_HOST" "$INFLUX_PORT"
